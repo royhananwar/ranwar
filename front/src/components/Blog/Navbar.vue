482f155283144c720f093e357e1f779e195a0ae1
Royhan Anwar <anwarroyhan@gmail.com>
2018-12-30 21:31:08 +0700

    add components HomePage, Navbar, Header, Footer setting vue-router

---
+++ front/src/components/Blog/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="javascript:;" @click="goHome()">Home</a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:;" @click="goBlog()">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:;" @click="goAbout()">Animals</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:;" @click="goAnimals()">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    app: 'BlogNavbar',
+    methods: {
+        goHome () {
+            this.$router.push('/')
+        },
+        goBlog() {
+            this.$router.push('/blog')
+        },
+        goAbout() {
+            this.$router.push('/about')
+        },
+        goAnimals() {
+            this.$router.push('/animals')
+        }
+    }
+}
+</script>
+
--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/Blog/HomePage.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+    { 
+        path: '/hello', 
+        component: HelloWorld 
+    },
+    {
+        path: '/',
+        component: HomePage 
+    },
+]
+
+const router = new VueRouter({
+    routes, // short for routes: routes
+    mode: 'history'
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    components: { App },
+    router,
+    render: h => h(App)
 })
